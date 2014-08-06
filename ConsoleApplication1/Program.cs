548be89f58113ec624b0f8ab7142107bb034469b
Sandip Patel <sandip.patel@lab49.com>
2014-08-06 16:58:01 -0400

    Added a test project to demonstrate temp queue issue with service stack rabbit mq wrappers.

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,64 @@
+﻿using ServiceStack.RabbitMq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using ServiceStack;
+using ServiceStack.Messaging;
+
+namespace ConsoleApplication1
+{
+    public class Query
+    {
+        public int Value { get; set; }
+    }
+
+    public class Response
+    {
+        public string Message { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stop = false;
+
+            var clientThread = new Thread(() =>
+            {
+                var mqServer = new RabbitMqServer();
+                var random = new Random();
+
+                var mqClient = mqServer.CreateMessageQueueClient();
+
+                int publishCount = 0, recieveCount = 0;
+
+                var tmpQueue = mqClient.GetTempQueueName();
+
+                while (!stop)
+                {
+                    Thread.Sleep(1000);
+
+                    mqClient.Publish(new Message<Query>(new Query { Value = random.Next(100) }) { ReplyTo = tmpQueue });
+                    Console.WriteLine("Sent a query message");
+                    publishCount++;
+
+                    var responseMessage = mqClient.Get<Response>(tmpQueue);
+                    Console.WriteLine("Recieved a response");
+                    if (responseMessage != null)
+                    {
+                        recieveCount++;
+                        Console.WriteLine(string.Format("PublishCount: {0}, RecieveCount: {1}, Response: {2}",
+                            publishCount, recieveCount, responseMessage.GetBody().Message));
+                    }
+                }
+            }) { IsBackground = true };
+
+            clientThread.Start();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ss-mq-test1/Program.cs
@@ -0,0 +1,67 @@
+﻿using ServiceStack.RabbitMq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+using RabbitMQ.Util;
+using ServiceStack;
+using ServiceStack.Messaging;
+
+namespace ss_mq_test1
+{
+    public class Query
+    {
+        public int Value { get; set; }
+    }
+
+    public class Response
+    {
+        public string Message { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stop = false;
+
+            var serverThread = new Thread(() =>
+            {
+                var mqServer = new RabbitMqServer();
+                var mqClient = mqServer.CreateMessageQueueClient();
+
+                while (!stop)
+                {
+                    var queryMessage = mqClient.Get<Query>(QueueNames<Query>.In);
+                    Console.WriteLine("Got a query message");
+                    if (queryMessage != null)
+                    {
+                        mqClient.Ack(queryMessage);
+                        var query = queryMessage.GetBody();
+                        var response = new Response { Message = string.Format("Query value: {0}, time: {1}", query.Value, DateTime.Now) };
+
+                        try
+                        {
+                            mqClient.Publish(
+                                queryMessage.ReplyTo,
+                                new Message<Response>(response));
+                            Console.WriteLine("Sent back a response");
+                        }
+                        catch (Exception exception)
+                        {
+                            Console.WriteLine("Exception: " + exception.Message);
+                        }
+                    }
+                }            
+            }) { IsBackground = true };
+
+
+            serverThread.Start();
+
+            Console.ReadLine();
+        }
+    }
+}
